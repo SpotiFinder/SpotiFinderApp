e95abe052089043f73c2e4815ed9570cd3103f62
Mariusz Maciejewski <maciejewski_mariusz@wp.pl>
2022-10-28 17:31:43 +0200

    initial project commit

---
+++ src/main/java/com/spotifinder/SpotifinderApplication.java
@@ -0,0 +1,13 @@
+package com.spotifinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotifinder/SpotifinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
